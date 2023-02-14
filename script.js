2e6ce801202bb6e40bf67e65bf3cc4bf7e885190
fredjacobs <thefredjacobs@gmail.com>
2023-02-14 13:16:57 +0200

    Add all functionality to game play
    Loop through conditions
    Evaluate condition to get winner or draw
    Print results to screen
    
    Forgot to commit and comment regularly
    Will remind self to do so more ;\

--- script.js
+++ script.js
@@ -11,7 +11,85 @@ function getPlayerSelection(){
     return prompt('Choose: Rock, Paper or Scissors').toLowerCase()
 }
 
-console.log(getPlayerSelection())
+function getWinner(computerSelection, playerSelection){
+
+    let winner  = "";
+
+    if (computerSelection === 'rock' && playerSelection === 'scissors'){
+        winner = 'computer'
+    } else if (computerSelection === 'rock' && playerSelection === 'paper'){
+        winner = 'player'
+    }else if (computerSelection === 'paper' && playerSelection === 'scissors'){
+        winner = 'player'
+    } else if (computerSelection === 'paper' && playerSelection === 'rock'){
+        winner = 'computer'
+    }else if (computerSelection === 'scissors' && playerSelection === 'paper'){
+        winner = 'computer'
+    } else if (computerSelection === 'scissors' && playerSelection === 'rock'){
+        winner = 'player'
+    }else{
+        winner = 'draw'
+    }
+
+    return {winner,computerSelection, playerSelection}
+
+}
+
+
+
+function playRound(){
+
+    const computerSelection = getComputerChoice()
+    const playerSelection = getPlayerSelection()
+
+    let roundInfo = getWinner(computerSelection, playerSelection)
+
+    return roundInfo
+
+
+}
+
+function game(){
+
+    let compScore = 0
+    let playerScore = 0
+    let winner = ""
+    
+
+     for(let i = 1; i <= 5; i++){
+
+        
+
+        roundInfo = playRound()
+
+        const winMessage = `You win! ${roundInfo.playerSelection} beats ${roundInfo.computerSelection}`
+        const looseMessage = `You loose! ${roundInfo.computerSelection} beats ${roundInfo.playerSelection}`
+        const drawMessage  = `Draw - ${roundInfo.computerSelection} = ${roundInfo.playerSelection}`
+
+        if (roundInfo.winner === 'computer'){
+            compScore++
+            console.log(looseMessage)
+            
+        }else if(roundInfo.winner === 'player'){
+            playerScore++
+            console.log(winMessage)
+        }else{
+            console.log(drawMessage)
+        }
+
+        console.log(`Comp Score: ${compScore} \nPlayer Score:  ${playerScore}`)
+        
+
+    }
+}
+
+game()
+
+
+
+
+
+
 
 
 
